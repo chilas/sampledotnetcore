66496bf296c9ad11068bcabde5c82960c9d0a07c
chilas <iamchilas@gmail.com>
2017-04-07 10:47:57 +0100

    chore: GitHubModel/Service setup
    setup basic project structure with models and services in place.

---
+++ SampleProject.BL/GitHubService.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using SampleProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SampleProject.Services
+{
+    public class GitHubService
+    {
+        public GitHubService()
+        {
+
+        }
+
+        public async Task<GitHubModel> GetUser(string username)
+        {
+            username = username ?? "chilas";
+
+            HttpClient client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://api.github.com/")
+            };
+            var response = await client.GetStringAsync($"users/${username}");
+            var user = JsonConvert.DeserializeObject<GitHubModel>(response);
+            return user;
+        }
+    }
+}
---
+++ SampleProject.Models/GitHubModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SampleProject.Models
+{
+    public class GitHubModel
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Blog { get; set; }
+        public string Location { get; set; }
+        public string Email { get; set; }
+        public string Bio { get; set; }
+        public int PublicRepos { get; set; }
+        public int Folowers { get; set; }
+        public int Following { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
